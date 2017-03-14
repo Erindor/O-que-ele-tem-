93bc13b9d499078812d7520675de602e192c1736
Bruno Cunha <rocks.bn@gmail.com>
2017-03-14 11:45:11 +0000

    Add página inicial do projeto

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="www/css/grid.css" />
+    <link rel="stylesheet" type="text/css" href="www/css/style1.css" />
+</head>
+
+<body>
+    <?php include_once "www/template/menu.php" ?>
+    
+    <div class='row'/>
+    
+    <div class='col-12 teste conteudo'>
+        fefefefe
+    </div>
+    
+    <?php include_once "www/template/footer.php" ?>
+</body>
+
+</html>
