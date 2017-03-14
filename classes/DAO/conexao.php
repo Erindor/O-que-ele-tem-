0932d36619654aa86f922ab51c0fcb512c446dad
Bruno Cunha <rocks.bn@gmail.com>
2017-03-14 23:17:49 +0000

    teste

--- classes/DAO/conexao.php
+++ classes/DAO/conexao.php
@@ -1,11 +1,11 @@
 <?php
 
 $host = "127.0.0.1";
-$user = "rocksbn";                     //Your Cloud 9 username
+$user = "rocksbn";                           //Your Cloud 9 username
 $pass = "";                                  //Remember, there is NO password by default!
-$db = "oqt";                                  //Your database name you want to connect to
+$db = "oqt";                                 //Your database name you want to connect to
 $port = 3306;                                //The port #. It is always 3306
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
-
+teste
 ?>
