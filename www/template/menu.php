6ddda397756867e67ea93a3a1b6ea41f7c0bae5e
Bruno Cunha <rocks.bn@gmail.com>
2017-03-21 21:58:35 -0300

    Menu responsivo

--- www/template/menu.php
+++ www/template/menu.php
@@ -1,5 +1,23 @@
+<!--menu principal-->
+<nav class="navbar navbar-default">
+   <div class="container-fluid">
 
- <div class='col-12 teste'>
-     Página1 - Página2 - Página3 - Página4
-     
-</div>
+      <div class="navbar-header">      
+         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menuSmart">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+         </button>      
+         <a class="navbar-brand" href="index.php"> O que ele tem? </a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="menuSmart">
+         <ul class="nav navbar-nav navbar-right">            
+            <li> <a href="index.php"> <span class="glyphicon glyphicon-home">    </span>  Início            </a> </li>
+            <li> <a href="#"> <span class="glyphicon glyphicon-user">            </span>  Sobre nós         </a> </li>
+            <li> <a href="#"> <span class="glyphicon glyphicon-question-sign">   </span>  FAQ               </a> </li>
+         </ul>
+      </div>
+
+   </div>
+</nav>
